76616ae79f356f7eb40d25c6eecda07ea2772abe
tkadjm <tkeane@chicagobooth.edu>
2015-07-29 00:01:32 -0500

    complete part 1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @user_number = params["number"].to_f
+    @user_sqrt = Math.sqrt(@user_number)
+    render("square_root.html.erb")
+  end
+
+  def random
+    @hi = params["hi"].to_i
+    @low = params["low"].to_i
+    @random = rand(@hi-@low) + @low
+    render("random.html.erb")
+  end
+
+  def payment
+    @rate = params["rate"].to_f / 10
+    @nper = params["nper"].to_i
+    @pv   = params["pv"].to_i
+    @pmt   = @pv * (@rate/1200*(1+@rate/1200)**(@nper*12)) / ((1+@rate/1200)**(@nper*12) - 1)
+    render("payment.html.erb")
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/",                         { :controller => "calculations", :action => "instructions"  })
+  get("/square_root/:number",      { :controller => "calculations", :action => "square_root"   })
+  get("/random/:low/:hi",          { :controller => "calculations", :action => "random"        })
+  get("/payment/:rate/:nper/:pv",  { :controller => "calculations", :action => "payment"       })
+
 end
